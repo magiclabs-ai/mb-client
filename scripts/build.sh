46c487a48509f14c815603042a372bc83c388d88
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-03-28 11:43:08 +0100

    [scripts] Update build & deploy-npm

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,4 @@
-npm run build
-cp ./package.json ./dist/package.json
+rm -rf ./dist
+tsc
+vite build
+sed 's/\.\//\.\//g; s/dist\///g' package.json > ./dist/pakage.json
--- scripts/deploy-npm.sh
+++ scripts/deploy-npm.sh
@@ -1,3 +1,3 @@
-source ./scripts/build.sh
+npm run build
 cp ./README.md ./dist/README.md
-npm deploy
+# npm deploy
