2c9abf67b686970ce3706d66322c2e7c63f9a0ae
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-03-30 10:16:36 +0100

    [models/image] Add ImageServer class

--- src/models/design-request/image.ts
+++ src/models/design-request/image.ts
@@ -24,3 +24,26 @@ export class Images {
     })
   }
 }
+export class ImageServer {
+  sfly_id: string
+  url: string
+  width: number
+  height: number
+  orientation: number
+  capture_time: string
+  camera_make?: string
+  camera_model?: string
+  filename: string
+
+  constructor(image: Image) {
+    this.sfly_id = image.handle
+    this.url = image.url
+    this.width = image.width
+    this.height = image.height
+    this.orientation = image.rotation
+    this.capture_time = image.captureTime
+    this.camera_make = image.cameraMake
+    this.camera_model = image.cameraModel
+    this.filename = image.filename
+  }
+}
