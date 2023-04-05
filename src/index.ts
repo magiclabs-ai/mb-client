ec1269f456d4781846c0cbb96eb10e02a26c7014
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-04-05 21:34:03 +0100

    [src/index] export type DesignRequest

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as MagicBookClient} from './models/client'
 export type {Image} from './models/image'
-export type {DesignRequestEvent, DesignRequestProps, DesignRequestEventDetail} from './models/design-request'
+export type {default as DesignRequest, DesignRequestEvent, DesignRequestProps, DesignRequestEventDetail}
+  from './models/design-request'
