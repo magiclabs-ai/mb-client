96d274784c1a8e72c64cc990bd9091a3f7d0d21b
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-05-09 19:48:40 +0100

    [workflows] Setup pre-release deployment

--- scripts/deploy-npm.sh
+++ scripts/deploy-npm.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+env_dev=false
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --dev)
+      env_dev=true
+      shift
+      ;;
+    *)
+      exit 1
+      ;;
+  esac
+done
+
 npm run build
 cd ./dist
-npm publish
+
+if [ $env_dev = true ]; then
+  sed -i 's/\("version": "\)\([^"]*\)\(".*\)/\1\2-dev\3/' ./package.json
+  npm publish --tag dev
+else
+  npm publish
+fi
+
