78fffedc5d29ec095787716aeb36b03fbb75ff51
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-05-20 01:19:36 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,2 @@
 export const apiHost = import.meta.env.VITE_API_HOST
 export const webSocketHost = import.meta.env.VITE_WEBSOCKET_HOST
-export const designRequestRefreshInterval = import.meta.env.VITE_DESIGN_REQUEST_REFRESH_INTERVAL
