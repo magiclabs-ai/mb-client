4eeebc36b8f5c294d39fecda5a0283c47323c54f
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-09-20 09:31:57 +0200

    [client] Export design-request data and galleon models

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,4 +1,6 @@
 export * from '@/core/models/client'
+export * from '@/core/models/design-request'
 export * from '@/core/models/design-request/image'
 export * from '@/core/models/design-request/design-options'
-export * from '@/core/models/design-request'
+export * from '@/core/models/galleon'
+export * from '@/core/data/design-request'
